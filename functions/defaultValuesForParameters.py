f8c32bbb0a7b03c9e9a0b72eaea8fdb6cb91cbc2
Jeferson Luis <jgjefersonluis@gmail.com>
2021-11-12 03:23:34 -0300

    Funcitons -> passing an argument e default values for parameters

---
+++ functions/defaultValuesForParameters.py
@@ -0,0 +1,6 @@
+def make_pizza (topping='pineapple'):
+    """ Make a single-topping pizza. """
+    print(f"Have a {topping} pizza!")
+
+make_pizza()
+make_pizza('mushroom')
---
+++ functions/passingAnArgument.py
@@ -0,0 +1,6 @@
+def greet_user (username):
+    """ Display a peseonalized greeting. """
+    print(f"Hello, {username}!")
+
+
+greet_user('Jesse')
