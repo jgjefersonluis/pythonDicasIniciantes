74b0452e382bd3723215bf5468f86a96a2ea28f2
Jeferson Luis <jgjefersonluis@gmail.com>
2021-11-16 02:54:28 -0300

    working tuples and 01-projectDogs build a list and print the items in the list

---
+++ workingTuples/tuples.py
@@ -0,0 +1,13 @@
+# Defining a tuple
+dimensions = (800, 600)
+
+# Looping through a tuple
+for dimensions in dimensions:
+    print(dimensions)
+
+# Overwriting a tuple
+dimensions = (800, 600)
+print(dimensions)
+
+dimensions = (1200,900)
+print(dimensions)
