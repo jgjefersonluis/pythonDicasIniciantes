9e8e8c420675e34f4ed2f9fa0f1b094b9389e04a
Jeferson Luis <jgjefersonluis@gmail.com>
2021-11-16 02:13:28 -0300

    list - min, max e sum simple statistics

---
+++ workingLists/simpleStatisticsMaximumValue.py
@@ -0,0 +1,5 @@
+ages = [93,99,66,17,85,1,35,82,2,77]
+oldest = max(ages)
+
+print(ages)
+print(oldest)
---
+++ workingLists/simpleStatisticsSumValue.py
@@ -0,0 +1,5 @@
+ages = [93,99,66,17,85,1,35,82,2,77]
+total_years = sum(ages)
+
+print(ages)
+print(total_years)
